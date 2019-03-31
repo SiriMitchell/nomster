ad4fd271d157fa4d2469d75ef0323c75e6e9e13e
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-03-31 15:12:52 +0000

    update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ class PhotosController < ApplicationController
 
     def create
       @place = Place.find(params[:place_id])
-      @place.photos.create(place_params.merge(user: current_user))
+      @place.photos.create(photo_params.merge(user: current_user))
       redirect_to place_path(@place)
     end
 
   private
 
     def photo_params
-      params.require(:photo).permit(:picture, :caption)
+      params.require(:photo).permit(:Picture, :caption)
     end
 end
