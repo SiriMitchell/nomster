ad58a7fef0d81a497b324d0b2fed146765d8cecc
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-04-05 20:00:12 +0000

    trying something else

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,7 @@ class PhotosController < ApplicationController
  before_action :authenticate_user!
 
     def create
+      @photo = Photo.new
       @place = Place.find(params[:place_id])
       @place.photos.create(photo_params.merge(user: current_user))
       redirect_to place_path(@place)
