914473c4b8964a8c2d73d2f10313c1826a7078a5
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-04-09 23:16:15 +0000

    test changed to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
 
-config.action_mailer.delivery_method = :test
+config.action_mailer.delivery_method = :smtp
 
  ActionMailer::Base.smtp_settings = {
    address: 'smtp.sendgrid.net',
